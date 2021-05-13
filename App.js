47d34f6d2f0487e89f638dbc8f8f2eda34454718
Ric0red <ricardoy1818@gmail.com>
2021-05-13 13:25:38 -0400

    modificacion del main app

--- App.js
+++ App.js
@@ -1,22 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+
+import {UserProvider} from './src/context/UserContext';
+import {FirebaseProvider} from './src/context/FirebaseContext';
+
+import AppStackScreen from './src/stack/AppStackScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>nueva app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <FirebaseProvider>
+      <UserProvider>
+        <NavigationContainer>
+          <AppStackScreen/>
+        </NavigationContainer>      
+      </UserProvider>      
+    </FirebaseProvider>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});  
- 
+};
